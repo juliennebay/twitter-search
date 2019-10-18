628b9fc8679f87db8cd5d4fa95e2a6ce312324a0
Julie <julienne.bay@gmail.com>
2019-10-18 17:48:54 -0400

    display text (mock tweets) from example search json

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const http = require("http")
-const fs = require("fs")
-const path = require("path")
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const FILES = {
-    ".js": "twitter-search.js",
-    ".css": "twitter-search.css"
-}
+  ".js": "twitter-search.js",
+  ".css": "twitter-search.css"
+};
 
 const requestHandler = (request, response) => {
-    console.log(`Request received to: ${request.url}`)
-    let contentType
-    let responseContent
+  console.log(`Request received to: ${request.url}`);
+  let contentType;
+  let responseContent;
 
-    if (request.url.includes("/search")){
-        contentType = "application/json"
-        const searchResults = require("./exampleSearchResult.json")
-        responseContent = JSON.stringify(searchResults)
+  if (request.url.includes("/search")) {
+    contentType = "application/json";
+    const searchResults = require("./exampleSearchResult.json");
+    responseContent = JSON.stringify(searchResults);
+  } else {
+    const fileName = FILES[path.extname(request.url)] || "index.html";
+    contentType = `text/${path.extname(request.url).replace(".", "") ||
+      "html"}`;
+    responseContent = fs.readFileSync(`./${fileName}`);
+  }
 
-    }else {
-        const fileName = FILES[path.extname(request.url)] || "index.html"
-        contentType = `text/${path.extname(request.url).replace(".", "") || "html"}`
-        responseContent = fs.readFileSync(`./${fileName}`)
-    }
+  response.writeHead(200, { "Content-Type": contentType });
+  response.end(responseContent, "utf-8");
+};
 
-    response.writeHead(200, { 'Content-Type': contentType });
-    response.end(responseContent, "utf-8")
-}
+const server = http.createServer(requestHandler);
 
-const server = http.createServer(requestHandler)
-
-server.listen(3000)
+server.listen(3000);
--- twitter-search.js
+++ twitter-search.js
@@ -1,11 +1,20 @@
-function loadScript(){
-    const button = document.querySelector("button")
+function loadScript() {
+  const button = document.querySelector("button");
+  const ul = document.querySelector("ul");
 
-    function search(){
-        const queryUrl = "http://localhost:3000/search"
-        fetch(queryUrl)
-    }
+  function search() {
+    const queryUrl = "http://localhost:3000/search";
+    fetch(queryUrl)
+      .then(response => response.json())
+      .then(result => {
+        result.statuses.forEach(statusObj => {
+          const li = document.createElement("li");
+          li.textContent = statusObj.text;
+          ul.appendChild(li);
+        });
+      });
+  }
 
-    button.addEventListener("click", search)
+  button.addEventListener("click", search);
 }
-document.addEventListener("DOMContentLoaded", loadScript)
+document.addEventListener("DOMContentLoaded", loadScript);
